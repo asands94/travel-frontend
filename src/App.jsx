615ffbe386cfdde2449526f386cc5a10732c128f
Ashley Sands <ashley.sands01@gmail.com>
2024-08-22 10:47:57 -0400

    setup basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import Trips from './pages/Trips'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='/' />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/trips'
+            element={
+              <ProtectedRoute>
+                <Trips />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/register' element={<RegisterAndLogout />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
