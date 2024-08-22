827534a36d4cc6b847a630971fef4c81ad7f5d07
Ashley Sands <ashley.sands01@gmail.com>
2024-08-22 11:27:52 -0400

    user can register and login

---
+++ src/components/Form.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import api from '../api'
+import { useNavigate } from 'react-router-dom'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
+
+function Form({ route, method }) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const name = method === 'login' ? 'Login' : 'Register'
+
+  const handleSubmit = async (e) => {
+    setLoading(true)
+    e.preventDefault()
+
+    try {
+      const res = await api.post(route, {
+        username: formData.username,
+        password: formData.password,
+      })
+      if (method === 'login') {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access)
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+        navigate('/trips')
+      } else {
+        navigate('/login')
+      }
+    } catch (e) {
+      alert(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>{name}</h1>
+      <input
+        type='text'
+        value={formData.username}
+        name='username'
+        onChange={handleChange}
+        placeholder='Username'
+      />
+      <input
+        type='password'
+        value={formData.password}
+        name='password'
+        onChange={handleChange}
+        placeholder='Password'
+      />
+      <button type='submit'>{name}</button>
+    </form>
+  )
+}
+
+export default Form
