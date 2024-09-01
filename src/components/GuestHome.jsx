bb8375c01cecd96fc197ba32e7c123ea13a6f218
Ashley Sands <ashley.sands01@gmail.com>
2024-09-01 13:51:59 -0400

    change hero image

--- src/components/GuestHome.jsx
+++ src/components/GuestHome.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import airplane from '../assets/airplane-woman-no-bg.png'
 function GuestHome() {
   return (
     <>
@@ -8,27 +9,26 @@ function GuestHome() {
             <h1 className='text-5xl font-semibold'>
               Plan, Explore, and Track Your Perfect Trip
             </h1>
-            <h2 className='text-3xl'>
+            <h2 className='text-2xl'>
               From itinerary planning to expense tracking, Trip Planner helps
               you stay organized and make the most of your adventures.
             </h2>
+            <button className='bg-primary rounded-full py-3 px-body mt-4 font-medium text-white w-1/5'>
+              <Link to='/register'>Sign Up</Link>
+            </button>
           </div>
           <img
             className='rounded-lg w-1/2'
-            src='https://api.time.com/wp-content/uploads/2023/06/travel-ai-help-01.jpg'
-            alt=''
+            src={airplane}
+            alt='woman sitting on airplane'
           />
         </div>
-
-        <button className='bg-primary rounded-full py-3 px-body mt-4 font-medium text-white'>
-          <Link to='/register'>Sign Up</Link>
-        </button>
       </section>
 
       <section className='bg-secondary flex flex-col items-center my-body'>
         <div className='flex justify-center w-full h-full px-body py-body'>
           <div className='flex flex-col items-center w-full'>
-            <h3 className='pb-5'>Add a Trip</h3>
+            <h3 className='pb-5 font-semibold'>Add a Trip</h3>
             <p className='shadow-md h-32 bg-pure w-full flex flex-col items-center rounded-lg p-5'>
               Effortlessly organize your next adventure by adding your
               destination, dates, and other essential trip details. View all
@@ -37,7 +37,7 @@ function GuestHome() {
           </div>
 
           <div className='flex flex-col items-center w-full mx-body'>
-            <h3 className='pb-5'>Add Your Itinerary</h3>
+            <h3 className='pb-5 font-semibold'>Add Your Itinerary</h3>
             <p className='shadow-md h-32 bg-pure w-full flex flex-col items-center rounded-lg p-5'>
               Map out your entire trip with a personalized itinerary. From
               must-see attractions to hidden gems, ensure every day is perfectly
@@ -46,7 +46,7 @@ function GuestHome() {
           </div>
 
           <div className='flex flex-col items-center w-full'>
-            <h3 className='pb-5'>Add Your Expenses</h3>
+            <h3 className='pb-5 font-semibold'>Add Your Expenses</h3>
             <p className='shadow-md h-32 bg-pure w-full flex flex-col items-center rounded-lg p-5'>
               Keep track of your spending with ease by logging your expenses.
               Stay within budget and enjoy peace of mind knowing exactly where
